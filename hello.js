29a60c669555be1fc697c802d862c72b20cb0626
Javier Cortejoso <javier.cortejoso@gmail.com>
2024-03-25 17:45:47 +0100

    Update hello.js
    Trigger CI/CD

--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from the APAC Office Hours in the terminal!";
 console.log(hello());
 exports.hello = hello;
+
